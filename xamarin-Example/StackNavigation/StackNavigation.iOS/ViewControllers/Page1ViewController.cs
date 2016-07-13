2a1f42df8506829951aa26c267f13e4a137c1f67
HilalSener <crescent.forest@hotmail.com>
2016-07-13 14:43:25 +0300

    Xamarin Stack Navigation Example

---
+++ xamarin-Example/StackNavigation/StackNavigation.iOS/ViewControllers/Page1ViewController.cs
@@ -0,0 +1,24 @@
+﻿using UIKit;
+
+namespace StackNavigation.iOS.ViewControllers
+{
+    public class Page1ViewController : UIViewController
+    {
+        public Page1ViewController()
+        {
+            this.View.BackgroundColor = UIColor.White;
+
+            this.Title = "White";
+
+            var myButton = UIButton.FromType(UIButtonType.RoundedRect);
+            myButton.SetTitle("Go to Next Page", UIControlState.Normal);
+            myButton.Frame = new CoreGraphics.CGRect(0, 0, 150, 30);
+            myButton.Center = this.View.Center;
+
+            myButton.TouchUpInside += (sender, e) =>
+            this.NavigationController.PushViewController(new Page2ViewController(), true);
+
+            this.Add(myButton);
+        }
+    }
+}
---
+++ xamarin-Example/StackNavigation/StackNavigation.iOS/ViewControllers/Page2ViewController.cs
@@ -0,0 +1,14 @@
+using UIKit;
+
+namespace StackNavigation.iOS.ViewControllers
+{
+    public class Page2ViewController: UIViewController
+    {
+        public Page2ViewController()
+        {
+            this.View.BackgroundColor = UIColor.Blue;
+
+            this.Title = "Blue";
+        }
+    }
+}
