dec8ffdf8d567521aacd36c8662403ba3f760108
Hilal Sener <crescent.forest@hotmail.com>
2016-07-21 11:31:46 +0300

    TableViewDemo project added

---
+++ Xamarin-Native/TableViewDemo/TableViewDemo/TableViewController.cs
@@ -0,0 +1,33 @@
+using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+
+namespace TableViewDemo
+{
+    public partial class TableViewController : UITableViewController
+    {
+
+		EmailServer emailServer = new EmailServer();
+
+        public TableViewController (IntPtr handle) : base (handle)
+        {
+			
+        }
+
+		public override nint RowsInSection(UITableView tableView, nint section)
+		{
+			return emailServer.Email.Count;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			UITableViewCell cell = new UITableViewCell(CGRect.Empty);
+
+			var emailItem = emailServer.Email[indexPath.Row];
+			cell.TextLabel.Text = emailItem.Subject;
+
+			return cell;
+		}
+    }
+}
---
+++ Xamarin-Native/TableViewDemo/TableViewDemo/ViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace TableViewDemo
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			
+		}
+
+		UITableView tableView;
+
+		class EmailServerDataSource : UITableViewSource
+		{
+			EmailServer emailServer = new EmailServer();
+
+			public override nint RowsInSection(UITableView tableview, nint section)
+			{
+				return emailServer.Email.Count;
+			}
+
+			public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+			{
+				UITableViewCell cell = new UITableViewCell(CGRect.Empty);
+
+				var emailItem = emailServer.Email[indexPath.Row];
+				cell.TextLabel.Text = emailItem.Subject;
+
+				return cell;
+			}
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			tableView = new UITableView(this.View.Frame);
+
+			tableView.Source = new EmailServerDataSource();
+
+			//this.View.Add(tableView);
+			//this.Add(tableView);
+			this.View.AddSubview(tableView);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+		}
+	}
+}
+
