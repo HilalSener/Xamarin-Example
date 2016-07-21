888eedb9a3a55db735fe7cf388f559e85639c9f4
Hilal Sener <crescent.forest@hotmail.com>
2016-07-21 10:53:24 +0300

    ListView demo added

---
+++ Xamarin-Native/ListViewDemo/ListViewDemo/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ListViewDemo
+{
+	[Activity(Label = "Instructors", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			var instructorListView = FindViewById<ListView>(Resource.Id.instructorListView);
+
+			instructorListView.ItemClick += OnItemClick;
+
+			var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1,
+			                              InstructorData.Instructors);
+			instructorListView.Adapter = adapter;
+		}
+
+		void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+		{
+			var instructor = InstructorData.Instructors[e.Position];
+
+			var dialog = new AlertDialog.Builder(this);
+			dialog.SetMessage(instructor.Name);
+			dialog.SetNeutralButton("Ok", delegate 
+			{
+			});
+			dialog.Show();
+		}
+	}
+}
+
+
