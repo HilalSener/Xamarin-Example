07efb5cc306985b3ba4275148ed877ac6ec7155c
Hilal Sener <crescent.forest@hotmail.com>
2016-07-27 17:46:24 +0300

    Project fixed

--- Xamarin-Forms/MobileApi/MobileApi/Views/DetailPage.xaml.cs
+++ Xamarin-Forms/MobileApi/MobileApi/Views/DetailPage.xaml.cs
@@ -10,28 +10,18 @@ namespace MobileApi
 {
 	public partial class DetailPage : ContentPage
 	{
-		StudentProvider provider = new StudentProvider();
-		List<Task> tasklist = new List<Task>();
-
 		public DetailPage()
 		{
 			InitializeComponent();
 
-			Task<List<Student>> studTask = Task<List<Student>>.Run(() => provider.Get().Result);
-			tasklist.Add(studTask);
-			Task.WaitAll(tasklist.ToArray());
-			if (studTask.Status == TaskStatus.RanToCompletion) {
-				var result = studTask.Result;
-				lvStudents.ItemsSource = result;
-			}
 
-			//List<Student> studList = new List<Student>()
-			//{
-			//	new Student() {Name = "Hilal", Surname = "ŞENER", About= "Jr. Xamarin Developer", RegistrationDate=DateTime.Now},
-			//	new Student() {Name = "Victor", Surname = "Tomaili", About= "Sr. Software Developer", RegistrationDate=DateTime.Now}
-			//};
+			List<Student> studList = new List<Student>()
+			{
+				new Student() {Name = "Hilal", Surname = "ŞENER", About= "Jr. Xamarin Developer", RegistrationDate=DateTime.Now},
+				new Student() {Name = "Victor", Surname = "Tomaili", About= "Sr. Software Developer", RegistrationDate=DateTime.Now}
+			};
 
-			//lvStudents.ItemsSource = studList;
+			lvStudents.ItemsSource = studList;
 		}
 	}
 }
