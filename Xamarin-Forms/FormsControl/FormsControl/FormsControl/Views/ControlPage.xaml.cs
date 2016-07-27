bd9ef327ca351ae8d50b10be95e0ff9d1901f77a
Hilal Sener <crescent.forest@hotmail.com>
2016-07-27 19:14:35 +0300

    ControlPage fixed

---
+++ Xamarin-Forms/FormsControl/FormsControl/FormsControl/Views/ControlPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsControl
+{
+	public partial class ControlPage : ContentPage
+	{
+		public ControlPage()
+		{
+			InitializeComponent();
+
+			myDatePicker.MinimumDate = DateTime.Now.AddYears(-1);
+			myDatePicker.MaximumDate = DateTime.Now.AddYears(1);
+			myDatePicker.Date = DateTime.Now;
+			myDatePicker.Format = "yyyy-MM-dd";
+		}
+
+		int i = 0;
+		private void onClicked(object sender, EventArgs e)
+		{
+			if (i % 2 == 0){
+				activity.IsRunning = true;
+			}
+
+		}
+	}
+}
+
