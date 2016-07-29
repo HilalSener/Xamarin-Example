2bee92f018c722d7a7795aef9093e19334ed8c5b
Hilal Sener <crescent.forest@hotmail.com>
2016-07-29 16:00:08 +0300

    Toggle(Switch), Time picker added.

--- Xamarin-Forms/FormsControl/FormsControl/FormsControl/Views/ControlPage.xaml.cs
+++ Xamarin-Forms/FormsControl/FormsControl/FormsControl/Views/ControlPage.xaml.cs
@@ -15,6 +15,33 @@ namespace FormsControl
 			myDatePicker.MaximumDate = DateTime.Now.AddYears(1);
 			myDatePicker.Date = DateTime.Now;
 			myDatePicker.Format = "yyyy-MM-dd";
+
+			//*** Web View Control Example ***
+			//Label header = new Label
+			//{
+			//	Text = "Web View",
+			//	FontSize = 50,
+			//	FontAttributes = FontAttributes.Bold,
+			//	HorizontalOptions = LayoutOptions.Center
+			//};
+
+			//WebView wView = new WebView
+			//{
+			//	Source = new UrlWebViewSource
+			//	{
+			//		Url = "https://www.acikakademi.com",
+			//	},
+			//	VerticalOptions = LayoutOptions.FillAndExpand
+			//};
+
+			//this.Content = new StackLayout
+			//{
+			//	Children =
+			//	{
+			//		header,
+			//		wView
+			//	}
+			//};
 		}
 
 		private void onClicked(object sender, EventArgs e)
@@ -35,6 +62,21 @@ namespace FormsControl
 		private void onStepperValueChanged(object sender, ValueChangedEventArgs e)
 		{
 			lblStepper.Text = e.NewValue.ToString();
+		}
+
+		private void onToggled(object sender, ToggledEventArgs e)
+		{
+			DisplayAlert("Switch", e.Value.ToString(), "Ok", "Cancel");
+
+			//bool isOk = await DisplayAlert("Switch", e.Value.ToString(), "Ok", "Cancel");
+			//if (isOk)
+			//{
+			//	//OK
+			//}
+			//else
+			//{ 
+			//	//CANCEL
+			//}
 		}
 	}
 }
