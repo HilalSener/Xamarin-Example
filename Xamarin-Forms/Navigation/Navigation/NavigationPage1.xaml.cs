6a0784caf0159ad53ae3549ba14ecc10761efe5b
Hilal Sener <crescent.forest@hotmail.com>
2016-08-30 16:26:14 +0300

    Xamarin.Forms Navigation project added.

---
+++ Xamarin-Forms/Navigation/Navigation/NavigationPage1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Navigation
+{
+	public partial class NavigationPage1 : ContentPage
+	{
+		public NavigationPage1()
+		{
+			InitializeComponent();
+			Title = "Main Page";
+			//Children.Add(new NavigationPage2());
+
+			Button btn = new Button()
+			{
+				Text = "Next Page",
+				TextColor = Color.Black,
+				FontSize = 20,
+				BorderWidth = 1,
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			btn.Clicked += btn_OnClicked;
+
+			Content = btn;
+		}
+
+		private async void btn_OnClicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new NavigationPage2());
+		}
+	}
+}
+
