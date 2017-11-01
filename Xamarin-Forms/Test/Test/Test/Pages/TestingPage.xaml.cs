775bc49dfb9288168f4143f62d50de68c5bc1f01
HilalSener <crescent.forest@hotmail.com>
2017-11-01 16:46:55 +0300

    Custom control test project added.

---
+++ Xamarin-Forms/Test/Test/Test/Pages/TestingPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestingPage : ContentPage
+    {
+        public TestingPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (width > height)
+            {
+                OuterStack.Orientation = StackOrientation.Horizontal;
+            }
+            else
+            {
+                OuterStack.Orientation = StackOrientation.Vertical;
+            }
+        }
+    }
+}
